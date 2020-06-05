419f76b3642f4c37ad8d66d206f3391e22cb8681
AnnKemp <ann.kemp@scarlet.be>
2020-06-05 12:33:22 +0200

    typescript string literals

--- greeter.js
+++ greeter.js
@@ -7,6 +7,25 @@ var Student = /** @class */ (function () {
     }
     return Student;
 }());
+// when the type can be anything you use any
+function debug(value) { }
+;
+debug("a string");
+debug(23);
+debug({ color: "blue" });
+// typescript literals
+var helloWorld = "Hello World";
+var hiWorld = "Hi World"; // this is a string because it is let
+// This function takes all strings
+function allowsAnyString(arg) { }
+;
+allowsAnyString(helloWorld);
+allowsAnyString(hiWorld);
+// This function only accepts the string literal "Hello World"
+function allowsOnlyHello(arg) { }
+;
+allowsOnlyHello(helloWorld);
+allowsOnlyHello(hiWorld); // this gives an error-message
 function greeter(person) {
     return "Hello, " + person.firstName + " " + person.lastName;
 }
--- greeter.ts
+++ greeter.ts
@@ -9,6 +9,26 @@ interface Person {
     firstName: string;
     lastName: string;
 }
+// when the type can be anything you use any
+function debug(value: any){};
+
+debug("a string");
+debug(23);
+debug({ color: "blue" });
+
+// typescript literals
+const helloWorld = "Hello World";
+let hiWorld = "Hi World"; // this is a string because it is let
+
+// This function takes all strings
+function allowsAnyString(arg: string){};
+allowsAnyString(helloWorld);
+allowsAnyString(hiWorld);
+
+// This function only accepts the string literal "Hello World"
+function allowsOnlyHello(arg: "Hello World"){};
+allowsOnlyHello(helloWorld);
+//allowsOnlyHello(hiWorld); // this gives an error-message in the terminal
 
 function greeter(person: Person) {
     return "Hello, " + person.firstName + " " + person.lastName;
